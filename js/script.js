0fbc3d2a45ba2110ea0f99034f59b4c3e182410e
Margo <plokhova.margarita@yandex.ru>
2020-10-18 18:53:18 +0300

    В html изменила путь подключения synopsis.js и в script.js вписала практическое задание 3, лекция 18

--- js/script.js
+++ js/script.js
@@ -26,9 +26,19 @@
 
 'use strict';
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
-let personalMovieDB = {
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+
+start();
+
+const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   actors: {},
@@ -82,29 +92,73 @@ personalMovieDB.movies[lastWatchedMovie2] = movieRating2; */
 // Код возьмите из предыдущего домашнего задания
 
 
-for (let i = 0; i < 2; i++) {
-  let lastWatchedMovie = prompt('Один из последних просмотренных фильмов?', '');
-  let movieRating = prompt('На сколько оцените его?', '');
 
-  if (lastWatchedMovie != null && movieRating != null && 
-    lastWatchedMovie != '' && movieRating != '' && lastWatchedMovie.length < 50) {
-    personalMovieDB.movies[lastWatchedMovie] = movieRating;
-    console.log('done');
-  } else {
-    console.log('error');
-    i--;
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    let lastWatchedMovie = prompt('Один из последних просмотренных фильмов?', '');
+    let movieRating = prompt('На сколько оцените его?', '');
+  
+    if (lastWatchedMovie != null && movieRating != null && 
+      lastWatchedMovie != '' && movieRating != '' && lastWatchedMovie.length < 50) {
+      personalMovieDB.movies[lastWatchedMovie] = movieRating;
+      console.log('done');
+    } else {
+      console.log('error');
+      i--;
+    }
   }
 }
 
-if (personalMovieDB.count < 10) {
-  console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-  console.log('Вы киноман');
-} else {
-  console.log('Произошла ошибка');
+// rememberMyFilms();
+
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+  } else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+  } else {
+    console.log('Произошла ошибка');
+  }
 }
 
-console.log(personalMovieDB);
+// detectPersonalLevel();
 
+
+
+// ПРАКТИЧЕСКОЕ ЗАДАНИЕ 3
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+// Код возьмите из предыдущего домашнего задания
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {    
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+}
+
+writeYourGenres();
