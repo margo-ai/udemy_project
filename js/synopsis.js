e8c18d59a30bcdd58cfd34295f863d7ddb68c738
Margo <plokhova.margarita@yandex.ru>
2020-10-18 21:46:07 +0300

    Лекция 20 о объектах и деструктуризации объектов

--- js/synopsis.js
+++ js/synopsis.js
@@ -330,3 +330,65 @@ function learnJS(lang, callback) {
  }
 
  learnJS('Javascript', done);
+
+
+ 
+
+
+ // ЛЕКЦИЯ 20 "ОБЪЕКТЫ, ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТОВ"
+
+
+ const options = {
+   name: 'test',
+   width: 1024,
+   heigth: 1024,
+   colors: {
+     border: 'black',
+     bg: 'red'
+   },
+   makeTest: function() { // Можно создавать методы в объектах самостоятельно
+     console.log("Test");
+   }
+ };
+
+ options.makeTest();
+
+// Деструктуризация объекта:
+const {border, bg} = options.colors;
+console.log(border);
+
+
+
+//  console.log(options.name);
+
+//  delete options.name;
+
+//  console.log(options);
+
+
+// Для счетчика свойств:
+let counter = 0;
+
+// for... in - цикл, проходящий через свойства объектов
+ for (let key in options) {
+   if (typeof(options[key]) == 'object') {
+     for (let i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+      counter++;
+     }
+   } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    counter++;
+   }  
+ }
+
+ console.log(counter);
+
+ // Методы - это действия, которые можно совершить с объектами
+
+ // Этот метод возвращает массив из перечисляемых свойств переданного обекта:
+console.log(Object.keys(options).length);
+// Этот метод возвращает массив собственных перечисляемых свойств указанного объекта в формате [key, value]:
+console.log(Object.entries(options));
+
+
