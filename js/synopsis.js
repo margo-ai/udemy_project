eda54ad683e887456aa22df949a68345839240e2
Margo <plokhova.margarita@yandex.ru>
2020-10-24 22:40:09 +0300

    Лекция 45 Функции-конструкторы

--- js/synopsis.js
+++ js/synopsis.js
@@ -998,3 +998,47 @@ loadScript("js/some.js");
     let end = new Date();
 
     alert(`Цикл отработал за ${end - start} миллисекунд`)
+
+
+
+
+
+
+    
+
+
+
+  //  ЛЕКЦИЯ 45 "ФУНКЦИИ-КОНСТРУКТОРЫ"
+
+// const num = new Number(3);
+// const num = new Function(3); // созданием новой функции мы создаем новый объект
+// console.log(num);
+
+// ниже - функция-коструктор, и с ее помощью можем создавать новых пользователей;
+// такие функции предназначены для конструирования объектов, создания множественных копий
+function User(name, id) {
+  this.name = name;
+  this.id = id;
+  this.human = true;
+  // помимо свойств можем помещать сюда и методы, например:
+  this.hello = function() {
+      console.log(`Hello ${this.name}`);
+  };
+}
+
+
+User.prototype.exit = function(name) {
+  console.log(`Пользователь ${this.name} ушел`);
+};
+
+
+const ivan = new User('Ivan', 28); // внутри переменной теперь не функция, а объект; с помощью new создаем новый объект
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
